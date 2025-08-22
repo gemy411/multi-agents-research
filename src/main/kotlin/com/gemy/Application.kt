c70183874a81e9585a705321062268e43fa5ac9b
gemy411 <mohamed11gamal96@gmail.com>
2025-08-22 03:15:11 +0300

    Use .env port

--- src/main/kotlin/com/gemy/Application.kt
+++ src/main/kotlin/com/gemy/Application.kt
@@ -1,11 +1,13 @@
 package com.gemy
 
+import io.github.cdimascio.dotenv.Dotenv
 import io.ktor.server.application.*
 import io.ktor.server.cio.*
 import io.ktor.server.engine.*
 
 fun main() {
-    embeddedServer(CIO, port = 8080, host = "0.0.0.0", module = Application::module)
+    val dotenv = Dotenv.load()
+    embeddedServer(CIO, port = dotenv.get("PORT")?.toIntOrNull() ?: 8080, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
 
